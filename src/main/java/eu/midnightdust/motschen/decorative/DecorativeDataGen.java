bb6f4d404243cf4b0df6e73acb487cbd663acefd
Martin Prokoph <motschen@midnightdust.eu>
2024-07-31 12:25:59 +0200

    Rocky Asphalt now generates naturally again

---
+++ src/main/java/eu/midnightdust/motschen/decorative/DecorativeDataGen.java
@@ -0,0 +1,52 @@
+package eu.midnightdust.motschen.decorative;
+
+import eu.midnightdust.motschen.decorative.world.OreFeatures;
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataOutput;
+import net.fabricmc.fabric.api.datagen.v1.provider.FabricDynamicRegistryProvider;
+import net.minecraft.registry.RegistryBuilder;
+import net.minecraft.registry.RegistryKeys;
+import net.minecraft.registry.RegistryWrapper;
+
+import java.util.concurrent.CompletableFuture;
+
+import static eu.midnightdust.motschen.decorative.DecorativeMain.MOD_ID;
+
+public class DecorativeDataGen implements DataGeneratorEntrypoint {
+    @Override
+    public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+        FabricDataGenerator.Pack pack = fabricDataGenerator.createPack();
+        pack.addProvider(WorldGenData::new);
+        System.out.println("Initialized dataGen");
+    }
+    @Override
+    public String getEffectiveModId() {
+        return MOD_ID;
+    }
+
+    @Override
+    public void buildRegistry(RegistryBuilder registryBuilder) {
+        System.out.println("building registry");
+        registryBuilder.addRegistry(RegistryKeys.CONFIGURED_FEATURE, OreFeatures::initConfigured);
+        registryBuilder.addRegistry(RegistryKeys.PLACED_FEATURE, OreFeatures::initPlaced);
+
+    }
+    public static class WorldGenData extends FabricDynamicRegistryProvider {
+        public WorldGenData(FabricDataOutput output, CompletableFuture<RegistryWrapper.WrapperLookup> registriesFuture) {
+            super(output, registriesFuture);
+        }
+
+        @Override
+        protected void configure(RegistryWrapper.WrapperLookup registries, Entries entries) {
+            System.out.println("configure");
+            entries.addAll(registries.getWrapperOrThrow(RegistryKeys.CONFIGURED_FEATURE));
+            entries.addAll(registries.getWrapperOrThrow(RegistryKeys.PLACED_FEATURE));
+        }
+
+        @Override
+        public String getName() {
+            return MOD_ID;
+        }
+    }
+}
--- src/main/java/eu/midnightdust/motschen/decorative/world/OreFeatures.java
+++ src/main/java/eu/midnightdust/motschen/decorative/world/OreFeatures.java
@@ -2,31 +2,52 @@ package eu.midnightdust.motschen.decorative.world;
 
 import eu.midnightdust.motschen.decorative.DecorativeMain;
 import net.fabricmc.fabric.api.biome.v1.BiomeModifications;
+import net.minecraft.registry.Registerable;
+import net.minecraft.registry.RegistryKey;
+import net.minecraft.registry.RegistryKeys;
+import net.minecraft.registry.entry.RegistryEntry;
+import net.minecraft.registry.tag.BiomeTags;
+import net.minecraft.registry.tag.BlockTags;
+import net.minecraft.structure.rule.TagMatchRuleTest;
 import net.minecraft.util.Identifier;
 import net.minecraft.world.gen.GenerationStep;
 import net.minecraft.world.gen.YOffset;
-import net.minecraft.world.gen.feature.*;
+import net.minecraft.world.gen.feature.ConfiguredFeature;
+import net.minecraft.world.gen.feature.Feature;
+import net.minecraft.world.gen.feature.OreFeatureConfig;
+import net.minecraft.world.gen.feature.PlacedFeature;
 import net.minecraft.world.gen.placementmodifier.CountPlacementModifier;
 import net.minecraft.world.gen.placementmodifier.HeightRangePlacementModifier;
 import net.minecraft.world.gen.placementmodifier.SquarePlacementModifier;
 
 import java.util.List;
 
+import static eu.midnightdust.motschen.decorative.DecorativeMain.id;
+import static eu.midnightdust.motschen.decorative.util.RegistryUtil.register;
+
 public class OreFeatures {
-//    private static final ConfiguredFeature<?, ?> ROCKY_ASPHALT_FEATURE = new ConfiguredFeature<>(Feature.ORE, new OreFeatureConfig(
-//                    OreConfiguredFeatures.STONE_ORE_REPLACEABLES, DecorativeMain.RockyAsphalt.getDefaultState(),10));
-//    public static PlacedFeature ROCKY_ASPHALT_PLACED_FEATURE = new PlacedFeature(
-//            RegistryEntry.of(ROCKY_ASPHALT_FEATURE),
-//            List.of(
-//                    CountPlacementModifier.of(40),
-//                    SquarePlacementModifier.of(),
-//                    HeightRangePlacementModifier.uniform(YOffset.BOTTOM, YOffset.TOP)
-//            ));
+    private static final ConfiguredFeature<?, ?> ROCKY_ASPHALT_FEATURE = new ConfiguredFeature<>(Feature.ORE, new OreFeatureConfig(
+            new TagMatchRuleTest(BlockTags.STONE_ORE_REPLACEABLES), DecorativeMain.RockyAsphalt.getDefaultState(),10));
+    public static PlacedFeature ROCKY_ASPHALT_PLACED_FEATURE = new PlacedFeature(
+            RegistryEntry.of(ROCKY_ASPHALT_FEATURE),
+            List.of(
+                    CountPlacementModifier.of(40),
+                    SquarePlacementModifier.of(),
+                    HeightRangePlacementModifier.uniform(YOffset.BOTTOM, YOffset.TOP)
+            ));
 
     public static void init() {
-//        Registry.register(BuiltinRegistries.CONFIGURED_FEATURE, new Identifier(DecorativeMain.MOD_ID, "rocky_asphalt"), ROCKY_ASPHALT_FEATURE);
-//        Registry.register(BuiltinRegistries.PLACED_FEATURE, new Identifier(DecorativeMain.MOD_ID, "placed_rocky_asphalt"), ROCKY_ASPHALT_PLACED_FEATURE);
-//        BiomeModifications.addFeature(biome -> (!biome.hasTag(BiomeTags.IS_NETHER) && !biome.hasTag(BiomeTags.END_CITY_HAS_STRUCTURE)),
-//                GenerationStep.Feature.UNDERGROUND_ORES, BuiltinRegistries.PLACED_FEATURE.getKey(OreFeatures.ROCKY_ASPHALT_PLACED_FEATURE).get());
+        BiomeModifications.addFeature(biome -> (!biome.hasTag(BiomeTags.IS_NETHER) && !biome.hasTag(BiomeTags.END_CITY_HAS_STRUCTURE)),
+                GenerationStep.Feature.UNDERGROUND_ORES, getKey(id("rocky_asphalt")));
+    }
+    public static RegistryKey<PlacedFeature> getKey(Identifier id) {
+        return RegistryKey.of(RegistryKeys.PLACED_FEATURE, id);
+    }
+
+    public static void initConfigured(Registerable<ConfiguredFeature<?, ?>> context) {
+        register(context, "rocky_asphalt", ROCKY_ASPHALT_FEATURE);
+    }
+    public static void initPlaced(Registerable<PlacedFeature> context) {
+        register(context, "rocky_asphalt", ROCKY_ASPHALT_PLACED_FEATURE);
     }
 }
