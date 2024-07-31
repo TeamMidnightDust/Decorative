1033dbc8b3e86a3d4c70b5c41e9287190bd12df9
Martin Prokoph <motschen@midnightdust.eu>
2024-07-31 21:09:58 +0200

    Include mod assets in Polymer packs

--- src/main/java/eu/midnightdust/motschen/decorative/polymer/PolymerSupport.java
+++ src/main/java/eu/midnightdust/motschen/decorative/polymer/PolymerSupport.java
@@ -1,9 +1,14 @@
 package eu.midnightdust.motschen.decorative.polymer;
 
 import eu.midnightdust.motschen.decorative.polymer.model.*;
+import eu.pb4.polymer.resourcepack.api.PolymerResourcePackUtils;
+
+import static eu.midnightdust.motschen.decorative.DecorativeMain.MOD_ID;
 
 public class PolymerSupport {
     public static void init() {
+        PolymerResourcePackUtils.addModAssets(MOD_ID);
+        
         DirectionalItemDisplayModel.initModels();
         SimpleItemDisplayModel.initModels();
         VariedItemDisplayModel.initModels();
